a1adcc685bde521e22b4086d940be1873dbfae28
gpsnmeajp <33391403+gpsnmeajp@users.noreply.github.com>
2023-07-12 00:27:22 +0900

    MMO

--- communication.js
+++ communication.js
@@ -19,8 +19,65 @@ async function postMemo(text) {
     relay.publish(event)
 }
 
+function upRGB(code){
+    let r = parseInt(code.substring(0, 2), 16)
+    let g = parseInt(code.substring(2, 4), 16)
+    let b = parseInt(code.substring(4, 6), 16)
+
+    r += 0x7F;
+    if(r>0xFF){r=0xFF;};
+    g += 0x7F;
+    if(g>0xFF){g=0xFF;};
+    b += 0x7F;
+    if(b>0xFF){b=0xFF;};
+
+    return r<<16 | g<<8 | b;
+}
+
 async function onMemo(event) {
-    const content = event.content;
+    let content = event.content;
+    content = content.replace(/(?:https?|ftp):\/\/[\n\S]+/g, ''); //URLを削除
+    content = content.replace(/(?:nostr):[\n\S]+/g, ''); //nostr:を削除
+
+    if (event.pubkey == mypubkey) {
+        player_text_sprite.text = content;
+        return;
+    }
+
+    let sprite = new PIXI.Text(content, {
+        fontFamily: 'Arial',
+        fontSize: 12,
+        fill: upRGB(event.pubkey),
+        align: 'center',
+        wordWrap: true,
+        wordWrapWidth: 200,
+        breakWords: true,
+    });
+    sprite.anchor.set(0.5, 0);
+
+    let picture = "unknown.png"
+    if (user_profiles[event.pubkey].picture != "") {
+        picture = user_profiles[event.pubkey].picture;
+    }
+    const sprite2 = PIXI.Sprite.from(picture);
+    sprite2.width = 24;
+    sprite2.height = 24;
+    sprite2.anchor.set(0.5, 1.0);
+
+    let x = (parseInt(event.sig.substring(0, 3), 16) - 0x800) /4; //次の目標座標
+    let y = (parseInt(event.sig.substring(3, 6), 16) - 0x800) /4;
+    let ox = x; //旧座標
+    let oy = y;
+
+    if (others[event.pubkey] != undefined) {
+        app.stage.removeChild(others[event.pubkey].text_sp);
+        app.stage.removeChild(others[event.pubkey].icon_sp);
+        ox = others[event.pubkey].x;
+        oy = others[event.pubkey].y;
+    }
+    others[event.pubkey] = { tx: x, ty: y, x: ox, y: oy, text_sp: sprite, icon_sp: sprite2, created_at: event.created_at};
+    app.stage.addChild(sprite);
+    app.stage.addChild(sprite2);
 }
 
 async function postPosition(x, y, z) {
@@ -45,9 +102,17 @@ async function postPosition(x, y, z) {
 }
 
 async function onPosition(event) {
-    const content = JSON.parse(event.content);
+    let content;
+    try {
+        content = JSON.parse(event.content);
+    } catch (e) {
+        return;
+    }
 
     // プロトコルバージョンチェック
+    if (content === undefined) {
+        return;
+    }
     if (content.v === undefined) {
         return;
     }
@@ -69,7 +134,7 @@ async function onPosition(event) {
 
 function getProfile(pubkey) {
     // 一旦初期値で埋めておく
-    if(user_profiles[pubkey] == undefined) {
+    if (user_profiles[pubkey] == undefined) {
         user_profiles[pubkey] = {
             name: pubkey,
             picture: ""
@@ -109,53 +174,75 @@ function onProfile(event) {
 }
 
 
-async function connectRelay() {
-    if(window.nostr == undefined){
+async function connectRelay(relay_adr) {
+    if (window.nostr == undefined) {
         setStatus("⚠ NIP-07 not found!");
         return;
     }
-    mypubkey = await window.nostr.getPublicKey();
 
-    // リレーに接続する
-    relay = window.NostrTools.relayInit('wss://relay-jp.nostr.wirednet.jp');
-    relay.on('connect', () => {
-        setStatus(`connected to ${relay.url}`)
-    });
-    relay.on('error', () => {
-        setStatus(`failed to connect to ${relay.url}`)
-    });
+    // リレーの情報を確認する
+    try {
+        const nip11 = await (await fetch(relay_adr.replace("wss://", "https://"), { headers: { "Accept": "application/nostr+json" } })).json();
+        console.log(nip11);
+        if (!nip11["supported_nips"].includes(16)) {
+            setStatus("⚠ This relay does not supported NIP-16!");
+            return;
+        }
+    } catch (e) {
+        setStatus("⚠ " + e);
+        return;
+    }
 
-    await relay.connect();
+    return new Promise(async (resolve, reject) => {
+        mypubkey = await window.nostr.getPublicKey();
 
-    // リレーから必要な情報を購読する
-    let sub = relay.sub([
-        {
-            kinds: [0, 1, 29420],
-            since: Math.round(Date.now() / 1000) - 15 * 60 // 15分前まで取得
-        }
-    ]);
-    sub.on('event', async event => {
-        if (event.kind === 29420) {
-            // 位置情報イベント
-            onPosition(event);
-        }
+        // リレーに接続する
+        relay = window.NostrTools.relayInit(relay_adr);
+        relay.on('connect', async () => {
+            setStatus(`connected to ${relay.url}`)
 
-        if (event.kind === 1) {
-            // メモイベント
-            onMemo(event);
-        }
+            // 自己Profileを取得
+            await getProfile(mypubkey);
+            resolve(mypubkey);
+        });
+        relay.on('error', () => {
+            setStatus(`failed to connect to ${relay.url}`)
+        });
 
-        if (event.kind === 0) {
-            // プロフィールイベント
-            onProfile(event);
-        }
-        let name = "unknown";
-        if(user_profiles[event.pubkey] == undefined) {
-            await getProfile(event.pubkey);
-        }
-        if(user_profiles[event.pubkey] != undefined) {
-            name = user_profiles[event.pubkey].name
-        }
-        addLog("[" + name + "] " + JSON.stringify(event));
+        await relay.connect();
+
+        // リレーから必要な情報を購読する
+        let sub = relay.sub([
+            {
+                kinds: [0, 1, 29420],
+                since: Math.round(Date.now() / 1000) - 3600 // 1時間前まで取得
+            }
+        ]);
+        sub.on('event', async event => {
+            if (user_profiles[event.pubkey] == undefined) {
+                await getProfile(event.pubkey);
+            }
+
+            if (event.kind === 29420) {
+                // 位置情報イベント
+                onPosition(event);
+            }
+
+            if (event.kind === 1) {
+                // メモイベント
+                onMemo(event);
+
+                let name = "unknown";
+                if (user_profiles[event.pubkey] != undefined) {
+                    name = user_profiles[event.pubkey].name
+                }
+                addLog("[" + name + "] " + event.content);
+            }
+
+            if (event.kind === 0) {
+                // プロフィールイベント
+                onProfile(event);
+            }
+        });
     });
 }
--- script.js
+++ script.js
@@ -3,18 +3,42 @@
 // python -m http.server
 // http://localhost:8000/
 
+// キャンバス
 let app = new PIXI.Application({ width: 800, height: 800 });
-let state_x = 0;
-let state_y = 0;
 
+// 自キャラクター状態
+let player = {
+    x: 0,
+    y: 0,
+    z: 0,
+    speed_x: 0,
+    speed_y: 0,
+    moved: false
+}
+
+// 自分のスプライト
+let player_icon_sprite;
+let player_text_sprite;
+
+// 他のスプライト
+let others = {};
+
+// ステータス表示を画面に反映
 function setStatus(s) {
     document.getElementById('status').innerText = s;
 }
-function setMyPosView(x, y, state_x, state_y) {
-    document.getElementById('pos').innerText = "(" + x + "," + y + ") [" + state_x + "," + state_y + "]";
+
+// ステータス表示を画面に反映
+function setPlayerState() {
+    document.getElementById('pos').innerText = JSON.stringify(player);
 }
+
+// ログを追加
 function addLog(s) {
-    document.getElementById('log').innerText = new Date() + " : " + s + '\n' + document.getElementById('log').innerText.substr(0, 4096);
+    console.log(s);
+
+    const el = document.getElementById('log');
+    el.innerText = new Date() + " : " + s + '\n' + el.innerText.substring(0, 4096);
 }
 
 // -------------------------------------------------------------------
@@ -36,8 +60,8 @@ function addLog(s) {
     let content = {
         x: x,
         y: y,
-        sx: state_x,
-        sy: state_y
+        sx: player.speed_x,
+        sy: player.speed_y
     };
 
     (async () => {
@@ -57,103 +81,193 @@ function addLog(s) {
     })();
 */
 
-
+// キー押されたとき
 document.addEventListener('keydown', (e) => {
     if (e.key === 'ArrowUp') {
-        state_y = -1;
+        player.speed_y = -1;
     }
     if (e.key === 'ArrowDown') {
-        state_y = 1;
+        player.speed_y = 1;
     }
     if (e.key === 'ArrowLeft') {
-        state_x = -1;
+        player.speed_x = -1;
     }
     if (e.key === 'ArrowRight') {
-        state_x = 1;
+        player.speed_x = 1;
     }
 });
+
+// キー離されたとき
 document.addEventListener('keyup', (e) => {
     if (e.key === 'ArrowUp' || e.key === 'ArrowDown') {
-        state_y = 0;
+        player.speed_y = 0;
+        player.moved = true;
     }
     if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
-        state_x = 0;
+        player.speed_x = 0;
+        player.moved = true;
     }
 });
+
+// タッチされたとき
 document.addEventListener('touchstart', (e) => {
     if (e.touches[0].pageY < window.innerHeight / 3) {
-        state_y = -1;
+        player.speed_y = -1;
     }
     if (e.touches[0].pageY > window.innerHeight * 2 / 3) {
-        state_y = 1;
+        player.speed_y = 1;
     }
     if (e.touches[0].pageX < window.innerWidth / 3) {
-        state_x = -1;
+        player.speed_x = -1;
     }
     if (e.touches[0].pageX > window.innerWidth * 2 / 3) {
-        state_x = 1;
+        player.speed_x = 1;
     }
 });
+
+// タッチ離されたとき
 document.addEventListener('touchend', (e) => {
-    state_y = 0;
-    state_x = 0;
+    player.speed_x = 0;
+    player.speed_y = 0;
+    player.moved = true;
 });
 
+// 画面読み込み時
 window.addEventListener('load', async (e) => {
-    document.getElementById('screen').appendChild(app.view);
-    app.resize(window.innerWidth, window.innerHeight);
-    let sprite = new PIXI.Text('[YOU]', {
-        fontFamily: 'Arial',
-        fontSize: 12,
-        fill: 0xFFFFFF,
-        align: 'center',
-    });
-    sprite.x = 400;
-    sprite.y = 400;
-    sprite.anchor.set(0.5);
-    app.stage.addChild(sprite);
-
-    let old_state_x = 0;
-    let old_state_y = 0;
-    app.ticker.add((delta) => {
-        if (state_x !== old_state_x || state_y !== old_state_y) {
-            old_state_x = state_x;
-            old_state_y = state_y;
-
-            setMyPosView(sprite.x, sprite.y, state_x, state_y);
+    // 送信エリアでCtrl + Enter
+    document.getElementById("message").addEventListener('keyup', async (e) => {
+        if (e.key === 'Enter' && e.ctrlKey) {
+            await sendText();
+            postPosition(player.x, player.y, player.z);
         }
-
-        sprite.y += state_y;
-        sprite.x += state_x;
-        /*
-                for(const c in users){
-                    let user = users[c];
-                    user.sprite.x = user.content.x;
-                    user.sprite.y = user.content.y;
-        
-                    // ステートに基づく予測座標更新
-                    user.content.x += user.content.sx;
-                    user.content.y += user.content.sy;
-                }
-        */
     });
-    // --------------------------------------------------------------------
-    connectRelay();
 
+    // 送信ボタン
     document.getElementById("send_button").addEventListener('click', async (e) => {
         await sendText();
+        postPosition(player.x, player.y, player.z);
     });
-    document.getElementById("message").addEventListener('keyup', async (e) => {
-        if(e.key === 'Enter' && e.ctrlKey){
-            await sendText();
+
+    // 変化があれば10秒おきに実行
+    setInterval(() => {
+        if (player.moved) {
+            player.moved = false;
+            postPosition(player.x, player.y, player.z);
+            console.log("Player Position Update!(10sec)");
         }
+    }, 10 * 1000);
+
+    // 変化がなくても60秒おきに実行
+    setInterval(() => {
+        player.moved = false;
+        postPosition(player.x, player.y, player.z);
+        console.log("Player Position Update!(60sec)");
+    }, 60 * 1000);
+
+    // リレー接続ボタン(初期化処理開始)
+    document.getElementById("relay_connect_button").addEventListener('click', async (e) => {
+        // 画面切り替え
+        document.getElementById('connect_relay').style = 'display:none';
+        document.getElementById('play_screen').style = 'display:block';
+
+        // リレーへの接続を開始
+        const relay_adr = document.getElementById("relay_address").value;
+        const mypubkey = await connectRelay(relay_adr);
+
+        // 初期位置を送信
+        player.x = (Math.random() - 0.5) * 100;
+        player.y = (Math.random() - 0.5) * 100;
+        postPosition(player.x, player.y, player.z);
+
+        // キャンバスの初期化を実施
+        document.getElementById('screen').appendChild(app.view);
+
+        // 自スプライトを作成(位置固定)
+        let picture = "unknown.png"
+        if (user_profiles[mypubkey].picture != "") {
+            picture = user_profiles[mypubkey].picture;
+        }
+
+        player_icon_sprite = PIXI.Sprite.from(picture);
+        player_icon_sprite.x = 400;
+        player_icon_sprite.y = 400;
+        player_icon_sprite.width = 64;
+        player_icon_sprite.height = 64;
+        player_icon_sprite.anchor.set(0.5, 1.0);
+        app.stage.addChild(player_icon_sprite);
+
+        player_text_sprite = new PIXI.Text("☆", {
+            fontFamily: 'Arial',
+            fontSize: 12,
+            fill: upRGB(mypubkey),
+            align: 'center',
+            wordWrap: true,
+            wordWrapWidth: 200,
+            breakWords: true,
+        });
+        player_text_sprite.x = 400;
+        player_text_sprite.y = 400;
+        player_text_sprite.anchor.set(0.5, 0);
+        app.stage.addChild(player_text_sprite);
+
+
+        // 描画ループ
+        app.ticker.add((delta) => {
+            setPlayerState();
+
+            // 自己状態を更新
+            player.x += player.speed_x * delta;
+            player.y += player.speed_y * delta;
+
+            // 他スプライトを更新
+            for (const o in others) {
+                let other = others[o];
+
+                // 相対位置を反映
+                other.text_sp.x = other.x - player.x + 400;
+                other.text_sp.y = other.y - player.y + 400;
+                other.icon_sp.x = other.x - player.x + 400;
+                other.icon_sp.y = other.y - player.y + 400;
+
+                // 透明度計算
+                let a = (3600.0 - (Math.floor(Date.now() / 1000) - other.created_at)) / 3600.0; //1時間で消える
+
+                // 移動
+                other.x = other.tx * 0.01 + other.x * 0.99;
+                other.y = other.ty * 0.01 + other.y * 0.99;
+
+                // kind 29420情報がある場合
+                if (user_positions[o] != undefined) {
+                    // 位置情報をオーバーライド
+                    other.tx = user_positions[o].x;
+                    other.ty = user_positions[o].y;
+                    // 位置情報が有効ならサイズを大きくする
+                    other.icon_sp.width = 64;
+                    other.icon_sp.height = 64;
+
+                    a = 1.0;
+                }
+                other.text_sp.alpha = a;
+                other.icon_sp.alpha = a;
+
+            }
+            /*
+            if(others.length > 3){
+                // バッファから削除
+                const child = others.shift();
+                // 表示から除去
+                app.stage.removeChild(child.text_sp);
+                app.stage.removeChild(child.icon_sp);
+            }
+            */
+        });
     });
 });
 
-async function sendText()
-{
+
+async function sendText() {
     let element = document.getElementById("message");
-    if(element.value.length === 0){return;}
+    if (element.value.length === 0) { return; }
 
     await postMemo(element.value);
     element.value = "";
